dd6a9a6486779a25a722097d50a97351f6a22537
carlbarfuss <carlbarfuss@gmail.com>
2020-12-17 10:17:53 -0600

    routing in app.js

---
+++ src/components/FavoriteView/FavoriteView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class FavoriteView extends Component {
+
+   render() {
+      return (
+         <div>
+          Welcome to  FavoriteView
+         </div>
+      );
+   }
+
+}
+
+export default FavoriteView;
