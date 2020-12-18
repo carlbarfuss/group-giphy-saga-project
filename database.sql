6cbd66696f64eb9c86fcff9d56299f69e7b8c828
carlbarfuss <carlbarfuss@gmail.com>
2020-12-18 11:37:36 -0600

    updated sql for table

--- database.sql
+++ database.sql
@@ -9,17 +9,16 @@ CREATE TABLE "category" (
     "name" VARCHAR (100) NOT NULL
 );
 
--- table for storing favorited gifs
+-- Default categories. You may change them :)
+INSERT INTO "category" ("name")
+VALUES ('unassigned'), ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
 CREATE TABLE "favorite" (
     id SERIAL PRIMARY KEY,
     url VARCHAR(200) NOT NULL,
     alt_text VARCHAR(200),
-    category_id INT
+    category_id INT DEFAULT 1
 );
 
 INSERT INTO "favorite" ("url", "alt_text")
-VALUES ('https://media1.giphy.com/media/KpLPyE3D6HJPa/200_d.gif?cid=aa0989470ffmrtqvhlpr8y1b03j83ppzgevma2jnb79otraa&rid=200_d.gif', 
-'mcdonalds GIF');
--- Default categories. You may change them :)
-INSERT INTO "category" ("name")
-VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+VALUES ('https://media1.giphy.com/media/KpLPyE3D6HJPa/200_d.gif?cid=aa0989470ffmrtqvhlpr8y1b03j83ppzgevma2jnb79otraa&rid=200_d.gif', 'mcdonalds GIF');
